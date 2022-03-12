9e3db41516b496d08671ce365846812826486871
SkemIgor <skemigor@mail.ru>
2022-03-12 18:25:06 +0300

    доработал

--- les-3_task-5.py
+++ les-3_task-5.py
@@ -4,10 +4,30 @@ nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
 adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
 adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
 
-def jokes(n):
 
-    for i in range(n):
-        joke = f'{random.choice(nouns)} {random.choice(adverbs)} {random.choice(adjectives)}'
-        print(joke)
+def jokes(quantity_of_jokes, replays=False):
+    """
+    returns a joke from 3 lists
+    :param quantity_of_jokes:
+    :param replays: If True no repeats
+    :return:
+    """
+    copy_joke = []
 
-jokes(3)
+    for i in range(quantity_of_jokes):
+
+        noun = random.choice(nouns)
+        adverb = random.choice(adverbs)
+        adjectiv = random.choice(adjectives)
+
+        joke = f'{noun} {adverb} {adjectiv}'
+        copy_joke.append(joke)
+
+        if replays:
+            nouns.remove(noun)
+            adverbs.remove(adverb)
+            adjectives.remove(adjectiv)
+
+    print(copy_joke)
+
+jokes(5, True)
